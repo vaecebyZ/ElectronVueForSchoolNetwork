31e13de664ca0001fd240e9aa73183a59630d29e
vaecebyZ <vaeceby2@qq.com>
2020-10-28 18:10:48 +0800

    init vue and elementui

---
+++ src/renderer/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div id="app">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'electron_for_school_network'
+  }
+</script>
+
+<style>
+  /* CSS */
+</style>
---
+++ src/renderer/components/index.vue
@@ -0,0 +1,16 @@
+
+<template>
+  <div>
+      <el-button>nihao</el-button>
+  </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
