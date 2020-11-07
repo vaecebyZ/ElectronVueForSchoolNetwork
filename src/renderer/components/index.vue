30aacbff965c9d9930abf2920f1d633c8816d71f
vaecebyZ <vaeceby2@qq.com>
2020-11-07 12:52:14 +0800

    新增消息界面+优化布局

--- src/renderer/components/index.vue
+++ src/renderer/components/index.vue
@@ -8,11 +8,22 @@
 </style>
 
 <style scoped>
-  .el-aside {
-    background-color: #D3DCE6;
+  #leftButton {
+    width: 200px !important;
+    color: rgb(80, 255, 27);
+    height: 137px;
+    background-color: black;
+    margin: 0px;
+    padding: 0px;
+  }
+
+
+  #topleft {
+    width: 200px !important;
     color: #333;
-    height: 543px;
-    width: 200px ! important;
+    height: 523px;
+    background-color: #D3DCE6;
+    
   }
 
   .el-main {
@@ -26,29 +37,39 @@
   <div>
     <el-container>
 
-      <el-aside>
+      <el-aside id="topleft">
 
         <div style="padding-top: 10%;padding-left: 10%;height: 70%;">
 
           <el-steps :active="active" direction="vertical" finish-status="success">
             <el-step title="连接测试"></el-step>
             <el-step title="网络测试"></el-step>
-            <el-step v-if="isStep" title="获取账号中"></el-step>
+            <el-step title="获取账号中"></el-step>
             <el-step title="心跳检测中♥..."></el-step>
           </el-steps>
 
-
-
         </div>
+
+        <el-aside id="leftButton">
+          <div>
+            <!-- v-infinite-scroll="load" -->
+            <ul id="teminial"  style="overflow:auto;list-style: none; margin: 0px;padding-left: 20px;">
+              <li v-for="(item,index) in count" :key="index" ><span
+                  style=" font-size:12px ;">{{count[index].time}}➡{{count[index].title}}</span></li>
+            </ul>
+
+          </div>
+        </el-aside>
+
 
       </el-aside>
 
       <el-main>
 
         <div>
-          <b style="position: absolute;top:523px;right: 5px;"><span class="yiyan"></span></b>
+          <b style="position: absolute;top:503px;right: 5px;"><span class="yiyan"></span></b>
         </div>
-        <el-button @click="addOne()">加一条</el-button>
+        <el-button>清除消息</el-button>
       </el-main>
 
     </el-container>
@@ -68,31 +89,67 @@
         pwd: '',
         isInit: true,
         active: 0,
-        isStep:false
+        count: []
       }
     },
-
+    watch: {
+      //实现数据更新后锁定最后一条数据
+      count: 'scrollToBottom'
+    },
     methods: {
+      //锁定方法
+      scrollToBottom: function () {
 
-        addOne(){
-            this.isStep =true;
-        },
-     
+        this.$nextTick(() => {
+
+          var div = document.getElementById('leftButton')
+
+          div.scrollTop = div.scrollHeight
+
+        })
+
+      },
+      //获取时间
+      msgPush(msgs){
+
+            let d = new Date();
+
+            let hour = d.getHours(); //得到小时数
+
+            let minute = d.getMinutes(); //得到分钟数
+
+            let second = d.getSeconds(); //得到秒数
+
+            let msg = {
+              time: hour + ":" + minute + ":" + second,
+              title: msgs 
+            }
+
+            this.count.push(msg)
+
+      },
+      addOne() {
+        this.count.push('233');
+        let msg = document.getElementById('teminial') // 获取对象
+        msg.scrollTop = msg.scrollHeight // 滚动高度
+      },
 
       //第一次测试--入口
       netWorkInit() {
 
-        this.active ++;
+        this.active++;
 
         this.$http.get('https://api.muxiaoguo.cn/api/yiyan', {}).then(res => {
 
+          
           //正确处理
           if (this.isInit) {
+            this.msgPush('网络连接正常✔');
             this.$notify({
-              title: '网络链接正常',
+              title: '网络链接正常✔',
               message: '网络正常,开始冲浪咯o(*￣▽￣*)ブ',
               type: 'success',
-              position: 'bottom-left'
+              //position: 'bottom-left'
             });
           }
 
@@ -102,11 +159,10 @@
           let conte = [];
 
           conte.push(data.constant + " ---" + data.source)
-
-          this.active++
+          this.active += 2;
 
           //屏蔽字符过长的语句
-          if (conte[0].length > 41) {
+          if (conte[0].length > 39) {
 
             console.log(conte[0].length);
 
@@ -130,12 +186,10 @@
           //请求失败
           console.log(error);
 
-          this.isStep=true;
-
           this.active++;
 
           this.$message({
-            message: '网卡未检测到连接',
+            message: '网卡未检测到连接❌',
             type: 'error'
           })
 
@@ -151,35 +205,38 @@
         this.user = UidPwd.User[i].name;
         this.pwd = UidPwd.User[i].pwd;
         //尝试登录
+        this.msgPush('尝试:'+this.user);
         this.$http.get('http://172.16.0.2/drcom/login?callback=dr1003&DDDDD=' + UidPwd.User[i].name + '%40cmcc&upass=' +
           UidPwd.User[i].pwd + '&0MKKey=123456&R1=0&R3=0&R6=0&para=00&v6ip=&v=7051', {}).then(res => {
           //返回反序列化
           let recode = JSON.parse(res.data.match(/[^\(\)]+(?=\))/g)[0]);
           // console.log(JSON.parse(res.data.match(/[^\(\)]+(?=\))/g)[0])); 
           if (recode.result == 1) {
-
+            this.msgPush('成功接入✔');
             this.$notify({
-              title: '成功接入',
-              message: '登上了倒霉蛋的号,开始冲浪咯o(*￣▽￣*)ブ',
+              title: '成功接入✔',
+              message: '登上了正确的号,开始冲浪咯o(*￣▽￣*)ブ',
               type: 'success',
-              position: 'bottom-left'
+             // position: 'bottom-left'
             });
+
+            this.active++;
 
             this.onHartBeat();
 
           } else {
-
+             this.msgPush('账号问题切账号❗');
             this.loginNet();
 
           }
         }).catch(error => {
 
           this.$message({
-            message: '网卡未检测到连接',
+            message: '网卡未检测到连接❌',
             type: 'error'
           })
 
-          console.log(error);
+         // console.log(error);
 
         });
       },
@@ -188,15 +245,23 @@
       onHartBeat() {
         setTimeout(() => {
           this.$http.get('https://api.muxiaoguo.cn/api/yiyan', {}).then(res => {
-            this.$message({
-              message: '网卡检测到连接',
-              type: 'success'
-            })
+            // this.$message({
+            //   message: '网卡检测到连接',
+            //   type: 'success'
+            // })
+         
+
+            
+
+            this.msgPush('心跳测试❤');
+            //console.log(msg.time);
+            // this.count.push(msg);
+
             this.onHartBeat();
           }).catch(error => {
 
             this.$message({
-              message: '网卡未检测到连接',
+              message: '网卡未检测到连接❌',
               type: 'error'
             })
 
@@ -207,12 +272,16 @@
       }
     },
     mounted() {
-      this.$message({
-        message: "网络测试中",
-        type: "warning"
-      })
-      this.active ++;
+
+      // this.$message({
+      //   message: "网络测试中",
+      //   type: "warning"
+      // })
+
+      this.msgPush('网络测试中...');
+
       setTimeout(() => {
+        this.active++;
         this.netWorkInit();
       }, 1500);
 
